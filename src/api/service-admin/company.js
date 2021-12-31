dcdb7e1f9ee77b9284bc594e16efdec16b81d0ee
littleredhat1997 <1656704949@qq.com>
2021-12-31 14:13:10 +0800

    delete: 删除无关接口

--- src/api/service-admin/company.js
+++ src/api/service-admin/company.js
@@ -2,13 +2,6 @@ import request from '@/utils/request'
 
 const API = '/service-admin/api/v1/company'
 
-export function getCompanyById(id) {
-  return request({
-    url: API + '/' + id,
-    method: 'get'
-  })
-}
-
 export function list(data) {
   return request({
     url: API + '/list',
